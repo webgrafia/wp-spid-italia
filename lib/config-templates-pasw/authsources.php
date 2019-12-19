f96754d4cea83e1a29fc3d793c0cf08ada998173
marco <webgrafia@gmail.com>
2019-12-19 01:15:21 +0100

    debug

--- lib/config-templates-pasw/authsources.php
+++ lib/config-templates-pasw/authsources.php
@@ -15,7 +15,7 @@ $config = array(
         'certificate' => 'saml.crt',
         // The entity ID of this SP.
         // Can be NULL/unset, in which case an entity ID is generated based on the metadata URL.
-        'entityID' => null,
+        'entityID' => "https://la-scuola.it",
         // The entity ID of the IdP this should SP should contact.
         // Can be NULL/unset, in which case the user will be shown a list of available IdPs.
         'idp' => null,
